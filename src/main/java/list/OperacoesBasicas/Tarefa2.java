133e3f07301142931d4559195e00f67eb60d9ae9
Michael Mello <michaelkcms@gmail.com>
2024-05-09 01:49:57 -0300

    Initial commit

---
+++ src/main/java/list/OperacoesBasicas/Tarefa2.java
@@ -0,0 +1,16 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa2 {
+    //atributo
+    private  string descricao:
+
+    public Tarefa2(string descricao) {
+        this.descricao = descricao;
+    }
+
+    public string getDescricao() {
+        return descricao;
+    }
+
+
+}
